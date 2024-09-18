7402230faf6d1b1ea319e729cb486d1ad56c9b0b
Subhash1861815 <126289834+Subhash1861815@users.noreply.github.com>
2024-09-18 09:06:06 -0500

    reactDom

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+
+
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+// import reportWebVitals from "./reportWebVitals";
 
-createRoot(document.getElementById('root')).render(
-  <HashRouter>
-  <BrowserRouter>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  </HashRouter>
-)
+  </React.StrictMode>
+);
+
+reportWebVitals();
+
+// createRoot(document.getElementById('root')).render(
+//   <HashRouter>
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   </HashRouter>
+// )
